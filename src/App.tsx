58f26ad62c887b197a6af35e6a762783c372a519
jstrzyzykowski <jakub.strzyzykowski@gmail.com>
2024-06-15 14:20:23 +0200

    Initial setup

---
+++ src/App.tsx
@@ -0,0 +1,11 @@
+import 'App.scss';
+
+const App = () => {
+  return (
+    <div className="container">
+      <h1 className="title">Hello Vite + React!</h1>
+    </div>
+  );
+};
+
+export default App;
---
+++ vite.config.ts
@@ -0,0 +1,15 @@
+import { defineConfig } from 'vitest/config';
+import react from '@vitejs/plugin-react-swc';
+import tsconfigPaths from 'vite-tsconfig-paths';
+import svgr from 'vite-plugin-svgr';
+
+// https://vitejs.dev/config/
+export default defineConfig({
+  plugins: [react(), tsconfigPaths(), svgr({ include: '**/*.svg' })],
+  test: {
+    environment: 'jsdom',
+    globals: true,
+    setupFiles: './src/setupTests.ts',
+    watch: false,
+  },
+});
